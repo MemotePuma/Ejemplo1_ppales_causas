baf4b597097485454932d4e8a60d58d507273906
MemotePuma <mickey_araujo@yahoo.com.mx>
2024-10-25 21:42:38 -0600

    Calculo de covid

--- Calculo.R
+++ Calculo.R
@@ -1,2 +1,11 @@
 library(dplyr)
 library(openxlsx)
+
+ruta <- "D:/Conjuntos_de_datos/Defunciones_registradas/2022/conjunto_de_datos_defunciones_registradas_2022.CSV"
+
+dsdef <- read.csv(ruta)
+
+totcovid <- dsdef %>% filter(lista_mex=="06T") %>% summarise(totcas=n())
+
+totcovid
+
